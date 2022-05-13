a8f811502f16ec5fbf4874e4b76df5ce41919fa8
mateusz.stradomski <mateusz.stradomski@PK1-21-T>
2022-05-13 09:43:30 +0200

    lab7b1.1

---
+++ Lab7b/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        //TODO: change DESKTOP-123ABC\SQLEXPRESS
+        string connectionString = @"Data Source=DESKTOP-123ABC\SQLEXPRESS;Initial Catalog=blogdb;Integrated Security=True";
+
+        using (BloggingContext db = new BloggingContext(connectionString))
+        {
+            Console.WriteLine($"Database ConnectionString: {db.ConnectionString}.");
+
+            // Create
+            Console.WriteLine("Inserting a new blog");
+
+            db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+            db.SaveChanges();
+
+            // Read
+            Console.WriteLine("Querying for a blog");
+
+            Blog blog = db.Blogs
+                .OrderBy(b => b.Id)
+                .First();
+
+            // Update
+            Console.WriteLine("Updating the blog and adding a post");
+
+            blog.Url = "https://devblogs.microsoft.com/dotnet";
+            blog.Posts.Add(new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+            db.SaveChanges();
+
+            // Delete
+            Console.WriteLine("Delete the blog");
+
+            db.Remove(blog);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ lab7-B/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab7_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
